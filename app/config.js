82f1cc1ba3a46e5b1d5cab349618faf1a9daa86e
OmairMairaj <omairmairaj@gmail.com>
2025-02-16 23:37:33 +0500

    OMA

---
+++ app/config.js
@@ -0,0 +1,5 @@
+// export const apiEndpoint = 'http://localhost:3000';
+export const apiEndpoint = 'https://whitelightenterprises-backend.vercel.app';
+export const Web_URL = "https://insta-auto-gram-webapp.vercel.app/"
+
+
